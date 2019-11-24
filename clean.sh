0ab08e80aa27ff33207e9eb19074507df7548770
Mike Papper <mike@optimalam.com>
2019-11-23 18:16:48 -0800

    added supplies for clean

--- clean.sh
+++ clean.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm important.html rules.html house.html recreation.html ski.html skicalendar.html cleaners.html
-rm york/important.html york/rules.html york/house.html york/lease.html york/ad.html
+rm york/important.html york/rules.html york/house.html york/lease.html york/ad.html york/supplies.html
 
 exit 0
